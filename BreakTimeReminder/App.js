3a51e6de4814a952fc9acf6ce39fd342a290010a
SwarnadyutiSarkar <166729648+SwarnadyutiSarkar@users.noreply.github.com>
2024-07-15 14:06:48 +0530

    Update App.js

--- BreakTimeReminder/App.js
+++ BreakTimeReminder/App.js
@@ -1,7 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import AppNavigator from './navigation/AppNavigator';
 import React, { useEffect } from 'react';
 import AppNavigator from './navigation/AppNavigator';
 import * as Notifications from 'expo-notifications';
@@ -35,25 +31,3 @@ export default function App() {
 
   return <AppNavigator />;
 }
-
-export default function App() {
-  return <AppNavigator />;
-}
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
